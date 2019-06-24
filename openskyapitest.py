9b87db7338b7c9ecf089c13f43c3e7754fefaff5
john-g8cqx <johnhawes@tiscali.co.uk>
2019-06-24 21:01:51 +0100

    response filtered with geo box

--- openskyapitest.py
+++ openskyapitest.py
@@ -5,8 +5,13 @@ Created on Mon Jun 24 19:44:04 2019
 
 @author: john
 """
-
+minlat = 51.25
+maxlat = 51.75
+minlong = 0.5
+maxlong = 1.75
+bbox =[minlat,maxlat,minlong,maxlong]
 from opensky_api import OpenSkyApi
 api = OpenSkyApi()
-s = api.get_states()
-print(s)
+s = api.get_states(0,None,None,bbox)
+for state in s.states:
+    print(state)
